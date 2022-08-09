b0d5a2f3fa1a616d8cfaa29c44a654527cd7dc87
Thierry DECKER <mail@thierry-decker.com>
2022-08-09 18:02:11 +0200

    Reformat code

--- app/helpers/query_builders.py
+++ app/helpers/query_builders.py
@@ -1,9 +1,18 @@
 from sqlalchemy.inspection import inspect
 from sqlalchemy.orm import load_only
+from sqlalchemy.orm import Session
+
 from app.helpers.columns import sanitise
 
 
-def select(db, sm_model, pm_model, columns=None, limit=0, offset=0):
+def select(
+    db: Session,
+    sm_model,
+    pm_model,
+    columns=None,
+    limit=0,
+    offset=0,
+):
     mapper = inspect(sm_model)
     pk_columns = [
         column.name
